6aca45d0a2519453131c0f20779902df8a1b4350
yzz1986351704 <1986351704@qq.com>
2022-02-26 17:19:14 +0800

    2022年2月17日，19:41，更新了主界面上的天气播报按键 2022年2月19人，15:26，更新了一些天气获取方面的代码 2022年2月20日，20:27，更新了天气预报的基本功能 2022年2月21日，17:10,更新了天气预报方面的代码，可以预报明日天气 2022年2月23日，22:21分，更新了地图定位方面的一些代码 2022年2月24日，2:15分，更新了一些定位方法，我和长鸣

--- app/src/main/java/com/example/blind/WeatherAPI.java
+++ app/src/main/java/com/example/blind/WeatherAPI.java
@@ -31,83 +31,18 @@ import com.amap.api.location.AMapLocation;
 import com.amap.api.location.AMapLocationClient;
 import com.amap.api.location.AMapLocationClientOption;
 import com.amap.api.location.AMapLocationListener;
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+
+import c.t.m.g.gl;
+
 public class WeatherAPI {
-
-
-
-
-    public static class Getlocation extends Activity {
-        //声明AMapLocationClient类对象
-        public AMapLocationClient mLocationClient = null;
-
-        //声明AMapLocationClientOption对象
-        public AMapLocationClientOption mLocationOption = null;
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-
-            try {
-                mLocationClient = new AMapLocationClient(getApplicationContext());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            mLocationClient.setLocationListener(mLocationListener);
-
-            mLocationOption = new AMapLocationClientOption();
-            //设置定位模式为AMapLocationMode.Hight_Accuracy，高精度模式。
-            mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
-            //设置是否返回地址信息（默认返回地址信息）
-            mLocationOption.setNeedAddress(true);
-            //获取一次定位结果：
-            //该方法默认为false。
-            mLocationOption.setOnceLocation(true);
-            //设置是否允许模拟位置,默认为false，不允许模拟位置
-            mLocationOption.setMockEnable(false);
-
-            //给定位客户端对象设置定位参数
-            mLocationClient.setLocationOption(mLocationOption);
-            //启动定位
-            mLocationClient.startLocation();
-        }
-
-        //声明定位回调监听器
-        public AMapLocationListener mLocationListener = new AMapLocationListener() {
-            @Override
-            public void onLocationChanged(AMapLocation amapLocation) {
-                if (amapLocation != null) {
-                    if (amapLocation.getErrorCode() == 0) {
-                        //定位成功回调信息，设置相关消息
-                        amapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见定位类型表
-                        amapLocation.getLatitude();//获取纬度
-                        amapLocation.getLongitude();//获取经度
-                        amapLocation.getAccuracy();//获取精度信息
-                        amapLocation.getAddress();//地址，如果option中设置isNeedAddress为false，则没有此结果，网络定位结果中会有地址信息，GPS定位不返回地址信息。
-                        amapLocation.getCountry();//国家信息
-                        amapLocation.getProvince();//省信息
-                        amapLocation.getCity();//城市信息
-                        amapLocation.getDistrict();//城区信息
-                        amapLocation.getStreet();//街道信息
-                        amapLocation.getStreetNum();//街道门牌号信息
-                        amapLocation.getCityCode();//城市编码
-                        amapLocation.getAdCode();//地区编码
-                        Log.d("地区编码", "amapLocation.getAdCode()");
-                        amapLocation.getAoiName();//获取当前定位点的AOI信息
-                    } else {
-                        //显示错误信息ErrCode是错误码，errInfo是错误信息，详见错误码表。
-                        Log.e("AmapError", "location Error, ErrCode:"
-                                + amapLocation.getErrorCode() + ", errInfo:"
-                                + amapLocation.getErrorInfo());
-                    }
-                }
-            }
-        };
-    }
-
-
-
-
     private static String province;
     private static String city;
     //今日天气部分
@@ -173,7 +108,8 @@ public class WeatherAPI {
         return windpowertomorrow;
     }
 
-    private static void httpURLGETCase() {
+    public static void httpURLGETCase() {
+
         String methodUrl = "http://restapi.amap.com/v3/weather/weatherInfo?key=ba3bcf7df9cfdb7bd0edf810ed98fb93&city=340321&extensions=all";
         HttpURLConnection connection = null;
         BufferedReader reader = null;
@@ -225,6 +161,7 @@ public class WeatherAPI {
             e.printStackTrace();
         }
     }
+
 
 }
 
